e6c9c2c42df9adc5ade9d87587d51df7e3d7fd9b
Brad <brad@Bradleys-MacBook-Air.local>
2013-10-29 21:42:42 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+var express = require('express'), 
+	engine = require('ejs-locals'),
+	fs = require('fs'),
+	marked = require('marked'),
+	dir = require('node-dir');
+
+var app = express();
+
+var posts = [];
+
+dir.readFiles('posts', {
+    match: /.md$/,
+    exclude: /^\./
+    }, function(err, content, filename,next) {
+        if (err) throw err;
+        var fileInfo = fs.statSync(filename);
+        posts.push({post:marked(content), date:fileInfo.ctime});
+        next();
+    },
+    function(err, files){
+        if (err) throw err;
+        orderPosts();
+        console.log('finished reading files:',files);
+    });
+
+app.configure(function() {
+	app.use(app.router);
+	app.engine('ejs', engine);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'ejs');
+	app.use(express.static(__dirname + '/public'));
+});
+
+app.get('/', function(req, res){
+	res.render('index', { posts: posts });
+});
+
+app.get('/post/:name', function(req, res){
+	res.render('single', { post: posts[0]})
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
+
+/* Private Functions */
+orderPosts = function(){
+	posts.sort(function(a,b){
+		var c = new Date(a.date);
+		var d = new Date(b.date);
+		return c-d;
+	});
+}
