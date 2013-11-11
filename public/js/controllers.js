1c9502f7b4c0567b99d669ee4fd4054cd301b9b9
Brad <brad@bradleys-air.pk5001z>
2013-11-10 20:52:21 -0600

    working controller

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,7 +1,17 @@
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-  	console.log('got here');
-  }])
+  controller('MyCtrl1', ['$scope', '$http', '$sce',
+  	function($scope, $http, $sce) {
+	  	$http.get("/post-list", {})
+		.success(function(data, status, headers, config) {
+			//console.log(data);
+			for(var i = 0; i < data.length; i++){
+				data[i].post = $sce.trustAsHtml(data[i].post);
+			}
+			$scope.posts = data;
+		}).error(function(data, status, headers, config) {
+			console.log('failed');
+		});
+	}])
   .controller('MyCtrl2', [function() {
 
   }]);
